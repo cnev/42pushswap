a899999f514732b50893d5ef83017224a61889fb
cnev <orthana103@gmail.com>
2015-03-13 16:59:31 +0100

    test de base

--- test.c
+++ test.c
@@ -5,5 +5,12 @@ int main(void)
 	t_stack			*stack1 = NULL;
 	t_stack			*stack2 = NULL;
 
+	pushback_stack(&stack1, 5);
+	pushback_stack(&stack1, 2);
+	pushback_stack(&stack1, 3);
+	pushback_stack(&stack1, 7);
+	pushback_stack(&stack1, 1);
 
+	stack_display(head);
+	return (0);
 }
